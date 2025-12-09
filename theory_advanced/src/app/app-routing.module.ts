47925146c0b6eeb3b2a95359fa987b51769c25e3
manhns15 <manhns15@gmail.com>
2025-12-09 23:43:12 +0700

    commit-form

---
+++ theory_advanced/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+
+const routes: Routes = [
+  {
+    path: 'tdf',
+    component: TemplateDrivenFormComponent,
+  },
+  {
+    path: 'rf',
+    component: ReactiveFormComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
