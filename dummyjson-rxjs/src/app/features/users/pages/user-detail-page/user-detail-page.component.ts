3935a321f7a6b8de06d0580f8c62dd7b5480fbe9
manhns15 <manhns15@gmail.com>
2026-02-01 23:47:15 +0700

    test gemini

--- dummyjson-rxjs/src/app/features/users/pages/user-detail-page/user-detail-page.component.ts
+++ dummyjson-rxjs/src/app/features/users/pages/user-detail-page/user-detail-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserStoreService } from '../../services/user-store.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-user-detail-page',
@@ -6,4 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-detail-page.component.scss'],
   standalone: false,
 })
-export class UserDetailPageComponent {}
+export class UserDetailPageComponent implements OnInit {
+  readonly user$ = this.userStore.user$;
+  readonly loading$ = this.userStore.loading$;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userStore: UserStoreService,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.pipe(map((params) => params['id'])).subscribe((id) => {
+      if (id) {
+        this.userStore.loadUser(+id);
+      }
+    });
+  }
+}
--- dummyjson-rxjs/src/app/features/users/services/user-store.service.ts
+++ dummyjson-rxjs/src/app/features/users/services/user-store.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, finalize } from 'rxjs';
+import { User } from 'src/app/core/models/user.model';
+import { DummyjsonApiService } from 'src/app/core/services/dummyjson-api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserStoreService {
+  private readonly userSubject = new BehaviorSubject<User | null>(null);
+  private readonly loadingSubject = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  readonly user$ = this.userSubject.asObservable();
+  readonly loading$ = this.loadingSubject.asObservable();
+
+  constructor(private dummyjsonApiService: DummyjsonApiService) {}
+
+  loadUser(userId: number): void {
+    this.loadingSubject.next(true);
+    // Reset user data when loading new user to avoid showing old data
+    this.userSubject.next(null);
+
+    this.dummyjsonApiService
+      .getUser(userId)
+      .pipe(finalize(() => this.loadingSubject.next(false)))
+      .subscribe({
+        next: (user) => {
+          this.userSubject.next(user);
+        },
+        error: (err) => {
+          console.error('Error loading user:', err);
+        },
+      });
+  }
 }
